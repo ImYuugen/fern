896786706f65922f4af6aae06a044f8999947006
Yuugen <yuugenssb@proton.me>
2024-12-27 02:32:08 +0100

    feat(websocket): start work on heartbeat/initial con
    Signed-off-by: Yuugen <yuugenssb@proton.me>

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,18 @@
 #![allow(dead_code)]
-#![allow(unused_variables)]
+
+#[macro_use]
+extern crate num_derive;
 
 use log::info;
 
 mod api;
+mod websocket;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     #[cfg(debug_assertions)]
     if std::env::var("RUST_LOG").is_err() {
-        std::env::set_var("RUST_LOG", "trace");
+        std::env::set_var("RUST_LOG", "debug");
         info!("Launching Fern in debug mode, don't get banned!");
     }
     env_logger::init();
