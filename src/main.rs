9c345cabde0e46179ad271d90e9b47e4470e3235
Yuugen <yuugenssb@proton.me>
2025-02-21 14:33:03 +0100

    feat: bump to Rust 2024 (yay !)
    Signed-off-by: Yuugen <yuugenssb@proton.me>

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     #[cfg(debug_assertions)]
     if std::env::var("RUST_LOG").is_err() {
-        std::env::set_var("RUST_LOG", "debug");
+        unsafe { std::env::set_var("RUST_LOG", "debug") };
         log::info!("Launching Fern in debug mode, don't get banned!");
     }
     env_logger::init();
